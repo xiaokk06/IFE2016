a7b693a706c8dda9b0350d600a069c749ef36413
小kk06 <hxl1096170488@163.com>
2016-04-27 23:11:36 +0800

    20

---
+++ IFE stage2/task20/query.js
@@ -0,0 +1,14 @@
+(function(window,undefined){
+	window.onload=function(){
+		var Input=document.getElementById("inputData");
+		// console.log(Input);
+
+
+		function init(){
+			Input.style.height=60+'px';
+			Input.style.width=200+'px';
+		}
+
+		init();
+	};
+})(window);
