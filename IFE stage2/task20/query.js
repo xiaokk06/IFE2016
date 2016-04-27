2261c6fcdfebf121b96b39234a34336e40fbb8b7
小kk06 <hxl1096170488@163.com>
2016-04-27 23:42:24 +0800

    addEvent

--- IFE stage2/task20/query.js
+++ IFE stage2/task20/query.js
@@ -1,18 +1,37 @@
-(function(window,undefined){
-	window.onload=function(){
-		var Input=document.getElementById("inputData");
-		// console.log(Input);
+(function(window, undefined) {
+    window.onload = function() {
+        var Input = document.getElementById("inputData"),
+        keyword=document.getElementById('keyword'),
+        query=document.getElementById('query'),
+        insert=document.getElementById('insert');
+        // console.log(Input);
 
 
-		function init(){
-			Input.style.height=60+'px';
-			Input.style.width=200+'px';
-		}
+        function init() {
+            Input.style.height = 60 + 'px';
+            Input.style.width = 200 + 'px';
+        }
 
-		function insertData(){
-			
-		}
+        // addEvent
+        function addEvent(element, type, handler) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + type, handler);
+            } else {
+                element['on' + type] = handler;
+            }
+        }
 
-		init();
-	};
+        function insertData() {
+            var Data = document.getElementById('inputData').value,
+                resultArray = [];
+            resultArray = Data.split(',\n.、');
+            console.log(resultArray);
+        }
+
+        addEvent(insert,'click',inputData);
+
+        init();
+    };
 })(window);
