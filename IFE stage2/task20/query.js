b89c74b8b71eda27f3b316ce89727c763ea638a4
小kk06 <hxl1096170488@163.com>
2016-04-28 23:06:16 +0800

    undefined

--- IFE stage2/task20/query.js
+++ IFE stage2/task20/query.js
@@ -1,3 +1,17 @@
+// 因为 ecmascript 执行JS代码是从里到外，因此把全局变量window或jQuery对象传进来，
+// 就避免了到外层去寻找，提高效率。undefined在老一辈的浏览器是不被支持的，直接使用会报错，
+// js框架要考虑到兼容性，因此增加一个形参undefined。
+// 还有，不要用window.undefined传递给形参，有可能window.undefined被其他人修改了，
+// 最好就是甚么都不传，形参的undefined就是真正的undefined了
+// 在Ecmascript 3中，undefined是可变的，不是关键字，这意味着它的值可以被覆盖或者重新赋值。
+// 如undefined = true。值得庆幸的是在ECMASCRIPT 5严格模式（’use strict';）中解析器将抛出一个类型错误。
+// 因此我们需要保护我们的undefined,
+// var undefined = 8;  
+// (function( window ) {   
+//     alert(window.undefined); // 8  
+//     alert(undefined); // 8  
+// })(window);  
+// 以上代码就可以修改window.undefined从而导致访问不到真正的undefined
 (function(window, undefined) {
     window.onload = function() {
         var Input = document.getElementById("inputData"),
@@ -6,7 +20,6 @@
             insert = document.getElementById('insert'),
             wrap = document.getElementById('wrap');
         // console.log(Input);
-
 
         function init() {
             Input.style.height = 60 + 'px';
@@ -29,6 +42,8 @@
 
         function insertData() {
             var Data = Input.value.trim();
+            // 多个分隔符使用正则表达式即可 中括号内直接写
+            // 换行\n tab\t \,\，分别为英文和中文的逗号 即半角和全角 空格可以直接用空格表示
             resultArray = Data.split(/[ ,\n.\，、\t\ ]/);
             console.log(resultArray);
             // loopInsert(resultArray);
@@ -53,6 +68,9 @@
 
         function search() {
             var word = keyword.value.trim();
+            console.log(word);
+            console.log(word==null);
+            console.log(word===null);
             render(word);
         }
 
@@ -60,6 +78,7 @@
         // 进行查询 然后将匹配项替换为高亮标记的文本
         function render(str) {
             wrap.innerHTML = resultArray.map(function(d) {
+            	// 这里如果
                 if (str != null && str.length > 0) {
                     d = d.replace(new RegExp(str, "g"), "<span class='select'>" + str + "</span>");
                 }
