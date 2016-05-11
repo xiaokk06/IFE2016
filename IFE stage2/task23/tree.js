0e182d528813d0b8293e005e6b34a917d35ded77
小kk06 <hxl1096170488@163.com>
2016-05-11 17:43:32 +0800

    query

--- IFE stage2/task23/tree.js
+++ IFE stage2/task23/tree.js
@@ -21,14 +21,16 @@
             lock = false,
             timer = 300;
 
+        // deep search
         function deepSearch() {
             var stack = [];
             (function recurse(currentNode) {
                 if (currentNode !== null) {
                     stack.push(currentNode);
                     for (var i = 0; i < currentNode.children.length; i++) {
-                        recurse(currentNode.children[i]);
-                        // arguments.callee(currentNode.children[i]);
+                        // recurse(currentNode.children[i]);
+                        // Recursion
+                        arguments.callee(currentNode.children[i]);
                     }
                     // callback ? callback(currentNode) : null;
                 }
@@ -36,8 +38,29 @@
             nodeList = stack;
         }
 
+        // wide search
+        function wideSearch() {
+        	reset();
+            // need a queue to store parentNode
+            // so that other childNode can be pushed into the nodeList stack
+            var queue = [],
+                currentNode = treeRoot;
+            nodeList = [];
+            nodeList.push(currentNode);
+            while (currentNode) {
+                var length = currentNode.children.length;
+                for (var i = 0; i < length; i++) {
+                    queue.push(currentNode.children[i]);
+                }
+                // callback ? callback(currentNode) : null;
+                currentNode = queue.shift();
+                nodeList.push(currentNode);
+            }
+        }
+
         // animation function
         function animation(nodeList, keyword) {
+        	reset();
             lock = true;
             var keyword = keyword || null;
             (function show() {
@@ -56,9 +79,37 @@
             })();
         }
 
+        function checkInput() {
+            return $('.keyword').value.trim();
+        }
+
+        function reset() {
+            nodeList = [];
+            clearInterval(timer);
+            var divs = document.getElementsByTagName('div');
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].style.backgroundColor = '#fff';
+            }
+        }
+
         addEvent($('.deepSearch'), 'click', function() {
             deepSearch();
             animation(nodeList);
+        });
+
+        addEvent($('.wideSearch'), 'click', function() {
+            wideSearch();
+            animation(nodeList);
+        });
+
+        addEvent($('.query'), 'click', function() {
+            var keyword = checkInput();
+            if (keyword) {
+                deepSearch();
+                animation(nodeList, keyword);
+            } else {
+                alert("No Found!");
+            }
         })
     }
 }(window));
